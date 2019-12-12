1c1dabb2d14fac1a2c603d5b3be3f867db72e231
rjp <rjjjjjp1997@163.com>
2019-12-12 14:10:17 +0800

    update chapter 01

---
+++ src/chapter_01/test_01_07/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+// /*
+//     rjelar
+// /*
+//     rerar
+// */
+//     sjrelaj;
+// */
+
+int main(int argc, char* argv[]) {
+    std::cout << "this is test_01_07" << std::endl;
+    return 0;
+}
+
+/*
+    Here is the output of the program
+    
+    /home/rjp/Documents/CppLearning/src/chapter_01/test_01_07/main.cpp:9:5: error: ‘sjrelaj’ does not name a type
+     sjrelaj;
+    /home/rjp/Documents/CppLearning/src/chapter_01/test_01_07/main.cpp:10:2: error: expected unqualified-id before ‘/’ token
+    /home/rjp/Documents/CppLearning/src/chapter_01/test_01_07/main.cpp:10:2: error: expected constructor, destructor, or type conversion before ‘/’ token
+
+*/
